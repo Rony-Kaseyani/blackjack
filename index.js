07624521c23f67cd3a2b0cfa9abf46bd59cf5f38
Rony Kaseyani <rony.kasbuh@gmail.com>
2021-09-07 18:15:57 +0200

    added new card button, dispalyed cards & sum on interface

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
 let firstCard = 10;
-let secondCard = 11;
+let secondCard = 9;
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
 let message = "";
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
 
-if (sum <= 20) {
-  message = "Do you want to draw a new card? 🙂";
-} else if (sum === 21) {
-  message = "Wohoo! You've got Blackjack! 🥳";
-  hasBlackJack = true;
-} else {
-  message = "You're out of the game! 😭";
-  isAlive = false;
+function startGame() {
+  renderGame();
 }
 
-console.log(message);
+function renderGame() {
+  sumEl.textContent = "Sum: " + sum;
+  cardsEl.textContent = "Cards: " + firstCard + " " + secondCard;
+
+  if (sum <= 20) {
+    message = "Do you want to draw a new card?";
+  } else if (sum === 21) {
+    message = "You've got Blackjack!";
+    hasBlackJack = true;
+  } else {
+    message = "You're out of the game!";
+    isAlive = false;
+  }
+
+  messageEl.textContent = message;
+}
+
+function newCard() {
+  let card = 5;
+  sum += card;
+  renderGame();
+}
