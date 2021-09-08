fe959db599b678b5db435955dc04d3597a525f10
Rony Kaseyani <rony.kasbuh@gmail.com>
2021-09-08 10:09:03 +0200

    added cards to arrays and also added new functions to game

--- index.js
+++ index.js
@@ -1,20 +1,41 @@
-let firstCard = 10;
-let secondCard = 9;
-let sum = firstCard + secondCard;
+let cards = [];
+let sum = 0;
 let hasBlackJack = false;
-let isAlive = true;
+let isAlive = false;
 let message = "";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
 
+function getRandomCard() {
+  let randomCard = Math.floor(Math.random() * 13) + 1;
+
+  if (randomCard > 10) {
+    return 10;
+  } else if (randomCard === 1) {
+    return 11;
+  } else {
+    return randomCard;
+  }
+}
+
 function startGame() {
+  isAlive = true;
+  let firstCard = getRandomCard();
+  let secondCard = getRandomCard();
+  cards = [firstCard, secondCard];
+  sum = firstCard + secondCard;
   renderGame();
 }
 
 function renderGame() {
+  cardsEl.textContent = "Cards: ";
+
+  for (i = 0; i < cards.length; i++) {
+    cardsEl.textContent += cards[i] + " ";
+  }
+
   sumEl.textContent = "Sum: " + sum;
-  cardsEl.textContent = "Cards: " + firstCard + " " + secondCard;
 
   if (sum <= 20) {
     message = "Do you want to draw a new card?";
@@ -30,7 +51,8 @@ function renderGame() {
 }
 
 function newCard() {
-  let card = 5;
+  let card = getRandomCard();
   sum += card;
+  cards.push(card);
   renderGame();
 }
